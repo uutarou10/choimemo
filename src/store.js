14670960d923e2a8b96feda82aa672336c351477
Kota Nonaka <kota.kaicho@gmail.com>
2018-10-08 14:40:17 +0900

    Add redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import rootReducer from './module'
 import { createBrowserHistory } from 'history';
 
@@ -9,6 +10,7 @@ export const history = createBrowserHistory();
 export default createStore(
   connectRouter(history)(rootReducer),
   composeWithDevTools(applyMiddleware(
+    thunk,
     routerMiddleware(history),
   ))
 );
